ac767b5d23791a2eba742631d87aad5d4b4964e0
Ringo37 <yajima.haruhiko@gmail.com>
2025-12-10 12:06:38 +0900

    editorに変更

--- app/components/editor.tsx
+++ app/components/editor.tsx
@@ -1,13 +1,58 @@
+import { codeBlockOptions } from "@blocknote/code-block";
+import { BlockNoteSchema, createCodeBlockSpec } from "@blocknote/core";
 import { BlockNoteView } from "@blocknote/mantine";
 import { useCreateBlockNote } from "@blocknote/react";
 import { useComputedColorScheme } from "@mantine/core";
 
-import "@blocknote/mantine/style.css";
 import "@blocknote/core/fonts/inter.css";
+import "@blocknote/mantine/style.css";
 
 export default function Editor() {
-  const editor = useCreateBlockNote();
   const colorScheme = useComputedColorScheme("light");
+  const editor = useCreateBlockNote({
+    schema: BlockNoteSchema.create().extend({
+      blockSpecs: {
+        codeBlock: createCodeBlockSpec(codeBlockOptions),
+      },
+    }),
+    initialContent: [
+      {
+        type: "codeBlock",
+        props: {
+          language: "typescript",
+        },
+        content: [
+          {
+            type: "text",
+            text: "const x = 3 * 4;",
+            styles: {},
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+      },
+      {
+        type: "heading",
+        props: {
+          textColor: "default",
+          backgroundColor: "default",
+          textAlignment: "left",
+          level: 3,
+        },
+        content: [
+          {
+            type: "text",
+            text: 'Click on "Typescript" above to see the different supported languages',
+            styles: {},
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+      },
+    ],
+  });
 
   return <BlockNoteView editor={editor} theme={colorScheme} />;
 }
