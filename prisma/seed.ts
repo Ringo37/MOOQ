10eb21e50079a5978742133f7e889eb7a41b0dbf
Ringo37 <yajima.haruhiko@gmail.com>
2025-12-17 00:51:41 +0900

    fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcryptjs";
 
-import { PrismaClient } from "generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
